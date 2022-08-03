6f4b06af708246f4ba4db989a407b29a4c4285c2
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-03 09:44:17 +0200

    Import and render  MainView

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
+//this file's job is to bootstrap the React Code
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view';
 
 // Import statement to indicate bundling `./index.scss`
 import './index.scss';
@@ -8,9 +10,7 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
+      <MainView />
     );
   }
 }
