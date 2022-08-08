d980b54f305b2b8ab9b4034ca771eec31bdc346c
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-08 12:21:03 +0200

    Add logic in comments regarding RegistrationView button

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import axios from 'axios';
 
 import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -51,6 +52,8 @@ render() {
 
   /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
   if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+  //if (register button is clicked) return <RegistrationView />
 
   // Before the movies have been loaded
   if (movies.length === 0) return <div className="main-view" />;
