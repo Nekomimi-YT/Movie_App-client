6ea92622546dca2590789ee19a9b7965053a0afb
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-09 18:37:48 +0200

    Add react-bootstrap code to create card styling template

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,15 +1,25 @@
+// movie_api-client/src/components/movie-card/movie-card.jsx
+
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>
-      <img src = {movie.ImagePath} />
-      <p>Title: {movie.Title}
-      Release Year: {movie.ReleaseYear}
-      Genre: {movie.Genre.Name}</p>
-      </div>;
+
+    return (
+      <Card>
+        <Card.Img variant="top" src={movie.ImagePath} />
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.ReleaseYear}</Card.Text>
+          <Card.Text>{movie.Genre.Name}</Card.Text>
+          <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 }
 
