bf4ba6d382132cff42cf9d73320f82fba0b12465
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-11 12:29:56 +0200

    Implement react-bootstrap accordion within Card

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Accordion from 'react-bootstrap/Accordion';
 
 export class MovieView extends React.Component {
 
@@ -6,52 +9,27 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return ( 
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img src={movie.ImagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.Title}</span>
-        </div>
-        <div className="movie-release-year">
-          <span className="label">Release Year: </span>
-          <span className="value">{movie.ReleaseYear}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>
-          <span className="value">{movie.Description}</span>
-        </div>
-        <div className="movie-critic-rating">
-          <span className="label">Critic Rating: </span>
-          <span className="value">{movie.CriticRating}</span>
-        </div>
-        <div className="movie-audience-rating">
-          <span className="label">Audience Rating: </span>
-          <span className="value">{movie.AudienceRating}</span>
-        </div>
-        <div className="movie-genre-name">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.Genre.Name}</span>
-        </div>
-        <div className="movie-genre-description">
-          <span className="label">Genre Details: </span>
-          <span className="value">{movie.Genre.Description}</span>
-        </div>
-        <div className="movie-director-name">
-          <span className="label">Director: </span>
-          <span className="value">{movie.Director.Name}</span>
-        </div>
-        <div className="movie-director-name">
-          <span className="label">Director Bio: </span>
-          <span className="value">{movie.Director.Bio}</span>
-        </div>
-        <div className="movie-actors">
-          <span className="label">Actors: </span>
-          <span className="value">{movie.Actors}</span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-      </div>
+        <Card style={{ width: '100%' }}>
+          <Card.Img variant="top" src={movie.ImagePath} />
+          <Card.Body>
+            <Card.Title>{movie.Title} {movie.ReleaseYear}</Card.Title>
+            <Card.Text>Starring: {movie.Actors}</Card.Text>
+            <Card.Text>{movie.Description}</Card.Text>
+            <Card.Text>Critic Rating: {movie.CriticRating}</Card.Text>
+            <Card.Text>Audience Rating: {movie.AudienceRating}</Card.Text>
+            <Accordion flush>
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>Genre: {movie.Genre.Name}</Accordion.Header>
+                  <Accordion.Body>{movie.Genre.Description}</Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>Director: {movie.Director.Name}</Accordion.Header>
+                  <Accordion.Body>{movie.Director.Bio}</Accordion.Body>
+              </Accordion.Item>  
+            </Accordion>
+            <Button variant="link" onClick={() => { onBackClick(null); }}>Back</Button>
+          </Card.Body>
+        </Card>
     );
   }
 }
