070122b3ef6d22b87190fd05e3b4ce8ceacf0d32
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-11 19:06:09 +0200

    Consolidate import code

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import { Form, Button } from 'react-bootstrap';
 import './registration-view.scss';
 
 export function RegistrationView(props) {
