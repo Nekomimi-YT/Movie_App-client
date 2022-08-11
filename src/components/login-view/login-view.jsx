55068105ec363bf5340c6c6531461340d837ff77
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-11 20:30:15 +0200

    Change button variants, size and positioning

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import {RegistrationView} from '../registration-view/registration-view'
 import './login-view.scss';
 
 export function LoginView(props) {
@@ -13,6 +14,14 @@ export function LoginView(props) {
     console.log(username, password);
     // Send a request to the server for authentication, then call props.onLoggedIn(username)
     props.onLoggedIn(username);
+  };
+
+  const handleRegistrationView = (e) => {
+    console.log('register new user');
+    // Send a POST request to the server, then reroute to the login page again
+    return (
+      <RegistrationView />
+    )
   };
 
   return (
@@ -27,12 +36,12 @@ export function LoginView(props) {
           <Form.Label>Password:</Form.Label>
           <Form.Control type="password" onChange={e => setPassword(e.target.value)} />
         </Form.Group>
-        <Button variant="primary" size="md" type="submit" onClick={handleSubmit}>
+        <Button variant="secondary" size="sm" type="submit" onClick={handleSubmit}>
           Submit
         </Button>
       </Form>
-          <p>New to myFlix?</p>
-          <Button variant="link" type="button" /*onClick={() => Change to RegistrationView*/>Register</Button>
+          <p>New to myFlix?{' '} 
+          <Button variant="secondary" type="button" size="sm" onClick={handleRegistrationView}>Register</Button></p>
     </div>
   );
 }
