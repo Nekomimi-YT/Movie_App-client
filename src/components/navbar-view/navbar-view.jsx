4a3ed1de4131f67113f4c090449b3b8ff45325eb
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-11 23:43:17 +0200

    Change #home to # for link placeholder in Navbar.Brand

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -7,7 +7,7 @@ export function NavbarView() {
   return (
     <Navbar bg="dark" fixed="top">
       <Container>
-        <Navbar.Brand href="#home"><h1>myFlix movies</h1></Navbar.Brand>
+        <Navbar.Brand href="#"><h1>myFlix movies</h1></Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text className="text">
