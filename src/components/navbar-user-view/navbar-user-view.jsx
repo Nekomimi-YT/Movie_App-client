25579e0c89104eff16707464528b331d95795b34
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-22 17:57:59 +0200

    Change component structure to class and add method/ code to logout user

--- src/components/navbar-user-view/navbar-user-view.jsx
+++ src/components/navbar-user-view/navbar-user-view.jsx
@@ -1,7 +1,47 @@
 import React from 'react';
 import { Container, Navbar, NavDropdown } from 'react-bootstrap';
 
-export function NavbarUserView() {
+export class NavbarUserView extends React.Component {
+  
+  onLoggedOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({
+      user: null
+    });
+    location.reload();
+  }
+  render () {
+
+    return (
+      <Navbar variant="dark" bg="dark" fixed="top" expand="md">
+      <Container>
+        <Navbar.Brand href="#"><h1>myFlix Movies</h1></Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end">
+          <NavDropdown className="navbar-nav" title={<span class="text">My Profile</span>} id="basic-nav-dropdown">
+              <NavDropdown.Item href="#">Update Profile</NavDropdown.Item>
+              <NavDropdown.Item href="#">Edit Favorite Movies</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => { this.onLoggedOut(); }}>Logout</NavDropdown.Item>
+            </NavDropdown>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    );
+  }
+}
+
+
+
+/*export function NavbarUserView() {
+
+  function onLoggedOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({
+      user: null
+    });
+  }
 
   return (
     <Navbar variant="dark" bg="dark" fixed="top" expand="md">
@@ -12,10 +52,10 @@ export function NavbarUserView() {
           <NavDropdown className="navbar-nav" title={<span class="text">My Profile</span>} id="basic-nav-dropdown">
               <NavDropdown.Item href="#">Update Profile</NavDropdown.Item>
               <NavDropdown.Item href="#">Edit Favorite Movies</NavDropdown.Item>
-              <NavDropdown.Item href="#">Logout</NavDropdown.Item>
+              <NavDropdown.Item onClick={() => { onLoggedOut(); }}>Logout</NavDropdown.Item>
             </NavDropdown>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-} 
+} */
