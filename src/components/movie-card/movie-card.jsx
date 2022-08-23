5d32ab50af4ca0f3041893b0c17d74e5fef55d05
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-23 11:59:10 +0200

    Import Link from React Router DON and replace button linking code

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,24 +1,27 @@
 // movie_api-client/src/components/movie-card/movie-card.jsx
 
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
   render() {
-    const { movie, onMovieClick } = this.props;
+    const { movie } = this.props;
     const shortMD = movie.Description.slice(0, 100);
 
     return (
       <Card className="d-flex align-self-stretch m-2 box-shadow">
-        <Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous"/>
+        <Card.Img variant="top" src={ movie.ImagePath } crossOrigin="anonymous"/>
         <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.ReleaseYear}</Card.Text>
-          <Card.Text>{movie.Genre.Name}</Card.Text>
-          <Card.Text>{shortMD}...</Card.Text>
-          <Button onClick={() => onMovieClick(movie)} variant="link" className="openCard">Open{'>>'}</Button>
+          <Card.Title>{ movie.Title }</Card.Title>
+          <Card.Text>{ movie.ReleaseYear }</Card.Text>
+          <Card.Text>{ movie.Genre.Name }</Card.Text>
+          <Card.Text>{ shortMD }...</Card.Text>
+          <Link to={`/movies/${ movie._id }`}>
+            <Button variant="link" className="openCard">Open{'>>'}</Button>
+          </Link>
         </Card.Body>
       </Card>
     );
@@ -33,6 +36,5 @@ MovieCard.propTypes = {
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired
     })
-  }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
+  }).isRequired
 };
