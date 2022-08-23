a5da3e9e5bdfad8405395e24f93f6c17226ab785
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-23 12:08:06 +0200

    Add link from React Router DOM and remove Accordion from React Bootstrap

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, Button, Accordion } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Card, Button } from 'react-bootstrap';
 import './movie-view.scss';
 
 export class MovieView extends React.Component {
@@ -9,25 +10,15 @@ export class MovieView extends React.Component {
 
     return ( 
         <Card className= "m-2 box-shadow" style={{ width: '100%' }}>
-          <Card.Img variant="top" src={movie.ImagePath} crossOrigin="anonymous"/>
+          <Card.Img variant="top" src={ movie.ImagePath } crossOrigin="anonymous"/>
           <Card.Body>
-            <Card.Title>{movie.Title} {movie.ReleaseYear}</Card.Title>
-            <Card.Text>Starring: {movie.Actors}</Card.Text>
-            <Card.Text>{movie.Description}</Card.Text>
-            <Card.Text>Critic Rating: {movie.CriticRating}</Card.Text>
-            <Card.Text>Audience Rating: {movie.AudienceRating}</Card.Text>
-            <div>
-            <Accordion flush>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>Genre: {movie.Genre.Name}</Accordion.Header>
-                  <Accordion.Body>{movie.Genre.Description}</Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Director: {movie.Director.Name}</Accordion.Header>
-                  <Accordion.Body>{movie.Director.Bio}</Accordion.Body>
-              </Accordion.Item>  
-            </Accordion>
-            </div>
+            <Card.Title>{ movie.Title } { movie.ReleaseYear }</Card.Title>
+            <Card.Text>Starring: { movie.Actors }</Card.Text>
+            <Card.Text>{ movie.Description }</Card.Text>
+            <Card.Text>Critic Rating: { movie.CriticRating }</Card.Text>
+            <Card.Text>Audience Rating: { movie.AudienceRating }</Card.Text>
+            <Card.Text>Genre: { movie.Genre.Name }</Card.Text>
+            <Card.Text>Director: { movie.Director.Name }</Card.Text>
             <Button variant="link" className="closeCard" onClick={() => { onBackClick(null); }}>{'<<'}Back</Button>
           </Card.Body>
         </Card>
