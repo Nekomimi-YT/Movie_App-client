00767385e68a62ce1af658fc9a9ffaa832796157
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-27 19:43:01 +0200

    Add ComponentDidUpdate method - Update state when a user is logged in

--- src/components/navbar-user-view/navbar-user-view.jsx
+++ src/components/navbar-user-view/navbar-user-view.jsx
@@ -12,6 +12,16 @@ export class NavbarUserView extends React.Component {
     };
 }
 
+componentDidUpdate(prevProps) {
+  // Typical usage (don't forget to compare props):
+  if (this.props.user !== prevProps.user) {
+    console.log(this.props.user, prevProps.user);
+    this.setState({
+      user: this.props.user
+    })
+  }
+}
+
   onLoggedOut() {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
@@ -31,7 +41,7 @@ export class NavbarUserView extends React.Component {
           <Navbar.Brand href="#"><h1>myFlix Movies</h1></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse className="justify-content-end">
-            <NavDropdown className="navbar-nav" title={<span class="text">My Profile</span>} id="basic-nav-dropdown">
+            <NavDropdown className="navbar-nav" title={<span className="text">My Profile</span>} id="basic-nav-dropdown">
                 <Link to={'/users/profile'}>
                   <NavDropdown.Item>View Profile</NavDropdown.Item>
                 </Link>
