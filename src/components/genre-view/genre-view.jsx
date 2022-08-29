61e8c533ef46cc69589f4a97b8d707b573aa01c0
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-29 20:04:02 +0200

    Change div to Row surrounding genreList.map loop

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -1,19 +1,24 @@
-// movie_api-client/src/components/genre-view-view/genre-view.jsx
+// movie_api-client/src/components/genre-view/genre-view.jsx
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card } from 'react-bootstrap';
+import { Row, Button, Card } from 'react-bootstrap';
+import { OtherMoviesView } from '../other-movies-view/other-movies-view';
 import './genre-view.scss';
 
 export class GenreView extends React.Component {
   render() {
-    const { genre, onBackClick } = this.props;
-
+    const { genre, genreList, onBackClick } = this.props;
+    console.log(genreList);
     return (
       <Card className="d-flex align-self-stretch m-2 box-shadow">
         <Card.Body>
           <Card.Title>{ genre.Name }</Card.Title>
-          <Card.Text>{ genre.Description}</Card.Text>
+          <Card.Text>{genre.Description}</Card.Text>
+          <h4>All {genre.Name} movies:</h4>
+          <Row className="d-flex">
+            {genreList.map(movie => <OtherMoviesView obj={movie.ImagePath} key={movie._id} />)}
+          </Row>
             <Button variant="link" className="closeCard" onClick={() => { onBackClick(); }}>{'<<'} Back</Button>
         </Card.Body>
       </Card>
