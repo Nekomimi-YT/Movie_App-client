6e1d964347116d964cead4c12510f712dd2c301b
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-29 11:33:45 +0200

    Remove movie variable and add genre in to render constants and update propTypes with genre

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -7,14 +7,14 @@ import './genre-view.scss';
 
 export class GenreView extends React.Component {
   render() {
-    const { movie, onBackClick } = this.props;
+    const { genre, onBackClick } = this.props;
 
     return (
       <Card className="d-flex align-self-stretch m-2 box-shadow">
         <Card.Body>
-          <Card.Title>{ movie.Genre.Name }</Card.Title>
-          <Card.Text>{ movie.Genre.Description}</Card.Text>
-            <Button variant="link" className="closeCard" onClick={() => { onBackClick(); }}>Back{'<<'}</Button>
+          <Card.Title>{ genre.Name }</Card.Title>
+          <Card.Text>{ genre.Description}</Card.Text>
+            <Button variant="link" className="closeCard" onClick={() => { onBackClick(); }}>{'<<'}Back</Button>
         </Card.Body>
       </Card>
     );
@@ -22,11 +22,9 @@ export class GenreView extends React.Component {
 }
 
 GenreView.propTypes = {
-  movie: PropTypes.shape({
-    Genre: PropTypes.shape({
+    genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired
-    })
-  }).isRequired,
+    }).isRequired,
   onBackClick: PropTypes.func.isRequired
 };
