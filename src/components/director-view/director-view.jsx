8d879907d26d2235f7e641111291b7614a54b702
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-29 20:37:01 +0200

    Import Row and add styling code around directorList.map method

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -2,19 +2,24 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Card } from 'react-bootstrap';
+import { Row, Button, Card } from 'react-bootstrap';
+import { OtherMoviesView } from '../other-movies-view/other-movies-view';
 import './director-view.scss';
 
 export class DirectorView extends React.Component {
   render() {
-    const { director, onBackClick } = this.props;
-
+    const { director, directorList, onBackClick } = this.props;
+    console.log(directorList);
     return (
       <Card className="d-flex align-self-stretch m-2 box-shadow">
         {/*<Card.Img variant="top" src={ movie.ImagePath } crossOrigin="anonymous"/> POSSIBLE PHOTO ADD HERE*/}
         <Card.Body>
           <Card.Title>{ director.Name }</Card.Title>
           <Card.Text>{ director.Bio }</Card.Text>
+          <h4>{director.Name}'s movies:</h4>
+          <Row className="d-flex">
+            {directorList.map(movie => <OtherMoviesView obj={movie.ImagePath} key={movie._id} />)}
+          </Row>
             <Button variant="link" className="closeCard" onClick={() => { onBackClick(); }}>{'<<'} Back</Button>
         </Card.Body>
       </Card>
