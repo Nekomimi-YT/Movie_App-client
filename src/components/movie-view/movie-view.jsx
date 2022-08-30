7669537584e98972b6d8e21661f65902287244a5
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-30 23:27:48 +0200

    Remove unnecessary classes and add h2 tags to the Card.Title

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -9,25 +9,25 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return ( 
-        <Card className= "m-2 box-shadow" style={{ width: '100%' }}>
+        <Card className= "m-2 box-shadow">
           <Card.Img variant="top" src={ movie.ImagePath } crossOrigin="anonymous"/>
           <Card.Body>
-            <Card.Title>{ movie.Title } { movie.ReleaseYear }</Card.Title>
+            <Card.Title><h2>{ movie.Title } { movie.ReleaseYear }</h2></Card.Title>
             <Card.Text>Starring: { movie.Actors }</Card.Text>
             <Card.Text>{ movie.Description }</Card.Text>
             <Card.Text>Critic Rating: { movie.CriticRating }</Card.Text>
             <Card.Text>Audience Rating: { movie.AudienceRating }</Card.Text>
             <Card.Text>
               Genre: <Link to={`/genres/${movie.Genre.Name}`}>
-              <Button variant="link" className="linkColor">{ movie.Genre.Name }</Button>
+              <Button variant="link">{ movie.Genre.Name }</Button>
               </Link>
             </Card.Text>
             <Card.Text>
               Director: <Link to={`/directors/${movie.Director.Name}`}>
-                <Button variant="link" className="linkColor">{ movie.Director.Name }</Button>
+                <Button variant="link">{ movie.Director.Name }</Button>
               </Link>
             </Card.Text>
-            <Button variant="link" className="linkColor" onClick={() => { onBackClick(); }}>{'<<'} Back</Button>
+            <Button variant="link" onClick={() => { onBackClick(); }}>{'<<'} Back</Button>
           </Card.Body>
         </Card>
     );
