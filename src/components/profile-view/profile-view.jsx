e07f132b69fd351db54b1757367767b71566a1cd
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-30 13:47:26 +0200

    Update form button text to 'Submit'

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -19,20 +19,26 @@ export class ProfileView extends React.Component {
 
   render() {
     const { user, email, birthday, onBackClick } = this.props;
+    const navEmail = localStorage.getItem('email');
+    const navBirthday = localStorage.getItem('birthday');
+
     return (
       <Card className="d-flex align-self-stretch m-2 box-shadow">
         {/*<Card.Img variant="top" src={ movie.ImagePath } crossOrigin="anonymous"/> POSSIBLE PHOTO ADD HERE*/}
         <Card.Body>
           <Button variant="link" className="closeCard" onClick={() => { onBackClick(); }}>{'<<'}Back</Button>
-          <Card.Title>Your myFlix Profile</Card.Title>
+          <Card.Title>Update your myFlix Profile:</Card.Title>
+          <Card.Text>Change any field and click the submit button to update.</Card.Text>
           <Card.Text>Username: { user }</Card.Text>
           <Card.Text>Password: *******</Card.Text>
-          <Card.Text>Email: { email }</Card.Text>
-          <Card.Text>Birthday: { birthday }</Card.Text>
+          <Card.Text>Email: { navEmail }</Card.Text>
+          <Card.Text>Birthday: { navBirthday }</Card.Text>
           <Card.Text>Favorites: {  }</Card.Text>
           <Button variant="secondary" size="sm" type="button">Edit Profile</Button>
-          <h4>Warning! Clicking this button will DELETE YOUR PROFILE!</h4>
-          <Button variant="warning" size="sm" type="button">Unregister</Button>
+          <div>
+            <h4>Warning! Clicking this button will DELETE YOUR PROFILE!</h4>
+            <Button variant="warning" size="sm" type="button">Unregister</Button>
+          </div>
         </Card.Body>
       </Card>
     );
