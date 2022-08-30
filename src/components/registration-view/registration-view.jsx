f1395efcef03d0912166bcb836b8f8ddfff56890
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-30 22:15:30 +0200

    Add class fontInput to form inputs for font styling

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -79,25 +79,25 @@ export function RegistrationView(props) {
         <p />
         <Form.Group controlId="formUsername" className="reg-form-inputs">
           <Form.Label>Username:</Form.Label>
-          <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} />
+          <Form.Control className="inputFont" type="text" value={username} onChange={e => setUsername(e.target.value)} />
             {values.usernameErr && <p>{values.usernameErr}</p>}
         </Form.Group>
 
         <Form.Group controlId="formPassword" className="reg-form-inputs">
           <Form.Label>Password:</Form.Label>
-          <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          <Form.Control className="inputFont" type="password" value={password} onChange={e => setPassword(e.target.value)} />
           {values.passwordErr && <p>{values.passwordErr}</p>}
         </Form.Group>
 
         <Form.Group controlId="formEmail" className="reg-form-inputs">
           <Form.Label>Email:</Form.Label>
-          <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} />
+          <Form.Control className="inputFont" type="email" value={email} onChange={e => setEmail(e.target.value)} />
           {values.emailErr && <p>{values.emailErr}</p>}
         </Form.Group>
 
         <Form.Group controlId="formBirthday" className="reg-form-inputs">
           <Form.Label>Birthday:</Form.Label>
-          <Form.Control type="date" name="birthday" onChange={e => setBirthday(e.target.value)} />
+          <Form.Control className="inputFont" type="date" name="birthday" onChange={e => setBirthday(e.target.value)} />
         </Form.Group>
 
         <Button variant="secondary" size="sm" type="submit" onClick={handleSubmit}>
