d15733fce1bc94f117045c7177e8659f69526205
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-31 13:48:42 +0200

    Update navbar brand to L'sFlix

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -3,11 +3,9 @@ import { Container, Navbar } from 'react-bootstrap';
 import './navbar-view.scss'
 
 export function NavbarView() {
-
-  return (
-    <Navbar bg="dark" fixed="top">
+    return ( <Navbar bg="dark" fixed="top">
       <Container>
-        <Navbar.Brand href="#"><h1>myFlix movies</h1></Navbar.Brand>
+        <Navbar.Brand href="#"><h1>L'sFlix</h1></Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text className="text">
@@ -17,4 +15,4 @@ export function NavbarView() {
       </Container>
     </Navbar>
   );
-} 
+  }
