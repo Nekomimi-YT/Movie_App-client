71cc782bf6c0ef4de1a0a6fe9a7c9b63669cc524
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-31 10:49:09 +0200

    change user to object data instead of Username for app testing

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -50,10 +50,10 @@ export class MainView extends React.Component {
 
   onLoggedIn(authData) {
     this.setState({
-      user: authData.user.Username,
+      user: authData.user
     });
     localStorage.setItem('token', authData.token);
-    localStorage.setItem('user', authData.user.Username);
+    localStorage.setItem('user', authData.user);
     localStorage.setItem('email', authData.user.Email);
     localStorage.setItem('birthday', authData.user.Birthday);
     this.getMovies(authData.token);
