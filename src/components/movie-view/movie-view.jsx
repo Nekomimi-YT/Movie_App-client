eb7f0e7a92d9e36b43d264751dee0df84bfe1c2d
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-31 12:50:40 +0200

    Add addToFavorites method to add favorite movies

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,18 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Card, Button } from 'react-bootstrap';
 import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
-  render() {
-    const { movie, onBackClick } = this.props;
+  addToFavorites(user, movie) {
+    const accessToken = localStorage.getItem('token');
+    console.log(accessToken);
+    console.log(movie._id);
+    axios.post(`https://myflix-movieapp-bylisa.herokuapp.com/users/${user}/movies/${movie._id}`, {
+      headers: { Authorization: `Bearer ${accessToken}` }
+    })
+    .then(response => {
+      this.setState({
+        userData: response.data
+      });
+      console.log(userData);
+      localStorage.setItem('favorites', userData.favoriteMovies);
+    })
+    .catch(error => console.log(error));
+  }
 
+  render() {
+    const { user, movie, onBackClick } = this.props;
     return ( 
         <Card className= "m-2 box-shadow">
-          <Card.Img variant="top" src={ movie.ImagePath } crossOrigin="anonymous"/>
+          <Card.Img variant="top" src={ movie.ImagePath } style={{width: 270}} crossOrigin="anonymous"/>
           <Card.Body>
-            <Card.Title><h2>{ movie.Title } { movie.ReleaseYear }</h2></Card.Title>
+            <Card.Title>
+              <h2>{ movie.Title } { movie.ReleaseYear }</h2>
+              <Button variant="secondary" size="sm" onClick={() => { this.addToFavorites(user, movie); }}>Me Like!</Button>
+            </Card.Title>
             <Card.Text>Starring: { movie.Actors }</Card.Text>
             <Card.Text>{ movie.Description }</Card.Text>
             <Card.Text>Critic Rating: { movie.CriticRating }</Card.Text>
