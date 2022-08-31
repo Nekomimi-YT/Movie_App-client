7e2f67bd255dcc842e2bceafa0b1fa63c5b429ec
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-31 16:59:55 +0200

    Import FavoritesView into MainView

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -12,6 +12,7 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { DirectorView } from '../director-view/director-view';
 import { GenreView } from '../genre-view/genre-view';
+import { FavoritesView } from '../favorites-view/favorites-view';
 import './main-view.scss';
 
 
