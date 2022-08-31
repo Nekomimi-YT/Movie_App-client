616960798fa2914bc98a46de51f80692510c4d38
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-08-31 16:41:47 +0200

    Update Brand name

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -5,7 +5,7 @@ import './navbar-view.scss'
 export function NavbarView() {
     return ( <Navbar bg="dark" fixed="top">
       <Container>
-        <Navbar.Brand href="#"><h1>L'sFlix</h1></Navbar.Brand>
+        <Navbar.Brand href="#"><h1>LisFlix</h1></Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
           <Navbar.Text className="text">
