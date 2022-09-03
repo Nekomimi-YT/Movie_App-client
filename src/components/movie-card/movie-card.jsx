61c40d8cee9f7da0ed1dbe35091ea8c7fede845e
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-03 22:04:02 +0200

    Remove unnecessary Col import from react bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { Row, Col, Button, Card } from 'react-bootstrap';
+import { Row, Button, Card } from 'react-bootstrap';
 import './movie-card.scss';
 
 export class MovieCard extends React.Component {
