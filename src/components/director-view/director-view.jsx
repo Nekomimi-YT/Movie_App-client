bc7ede03b3584f5d72489e4f439cf00c90a7c5ff
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-04 10:20:15 +0200

    move back button and surrounding div up out of the Card.Body

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -13,6 +13,9 @@ export class DirectorView extends React.Component {
     return (
       <Card className="d-flex align-self-stretch m-2 box-shadow">
         {/*<Card.Img variant="top" src={ movie.ImagePath } crossOrigin="anonymous"/> POSSIBLE PHOTO ADD HERE*/}
+        <div>
+          <Button variant="link" onClick={() => { onBackClick(); }}>{'<<'} Back</Button>
+        </div>
         <Card.Body>
           <Card.Title><h2>{ director.Name }</h2></Card.Title>
           <Card.Text>{ director.Bio }</Card.Text>
@@ -20,7 +23,6 @@ export class DirectorView extends React.Component {
           <Row className="d-flex">
             {directorList.map(movie => <OtherMoviesView obj={movie.ImagePath} key={movie._id} />)}
           </Row>
-            <Button variant="link" onClick={() => { onBackClick(); }}>{'<<'} Back</Button>
         </Card.Body>
       </Card>
     );
