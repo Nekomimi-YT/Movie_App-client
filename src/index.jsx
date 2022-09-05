8411ac8d718c18cb58903129fa858dcb25a70f24
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-05 21:33:05 +0200

    Add redux, Provider and redux dev tools.  Wrap Container in Provider.

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,31 @@
 //this file's job is to bootstrap the React Code
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MainView } from './components/main-view/main-view';
-import Container from 'react-bootstrap/Container';
+import { Container } from 'react-bootstrap';
+import { createStore } from 'redux'; //deprecated.  configStore?
+import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
+import moviesApp from './reducers/reducers';
+
+import MainView from './components/main-view/main-view';
 
 // Import statement to indicate bundling `./index.scss`
 import './index.scss';
 
+const store = createStore(moviesApp, devToolsEnhancer());
+
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <Container>
-        <MainView />
-      </Container>
-    );
-  }
-}
+    return(
+      <Provider store={store}>
+         <Container>
+           <MainView />
+         </Container>
+       </Provider>
+     );
+   }
+ }
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
