c55bdce5985b4e6c1164d5123c12c5ebeb08ae18
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-06 12:26:18 +0200

    Remove unnecessary actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,6 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER =  'SET_FILTER';
-export const CREATE_USER = 'CREATE_USER';
 export const SET_USER = 'SET_USER';
-export const UPDATE_USER = 'UPDATE_USER';
-export const DEL_USER = 'DEL_USER';
 export const SET_FAVMOV = 'SET_FAVMOV';
 export const DEL_FAVMOV = 'DEL_FAVMOV';
 
@@ -18,18 +15,6 @@ export function setFilter(value) {
 
 export function createUser(value) {
   return { type: CREATE_USER, value };
-}
-
-export function setUser(value) {
-  return { type: SET_USER, value };
-}
-
-export function updateUser(value) {
-  return { type: UPDATE_USER, value };
-}
-
-export function deleteUser(value) {
-  return { type: DEL_USER, value };
 }
 
 export function setFavmov(value) {
