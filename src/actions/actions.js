96c2e9c0eb18021f4c1dbe67e2a063126ee9db54
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-06 12:40:33 +0200

    Update setUser parameter to user and change/ createUser function to setUser

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,9 +1,8 @@
 export const SET_MOVIES = 'SET_MOVIES';
 export const SET_FILTER =  'SET_FILTER';
 export const SET_USER = 'SET_USER';
-export const SET_FAVMOV = 'SET_FAVMOV';
+export const ADD_FAVMOV = 'SET_FAVMOV';
 export const DEL_FAVMOV = 'DEL_FAVMOV';
-
 
 export function setMovies(value) {
   return { type: SET_MOVIES, value };
@@ -13,12 +12,12 @@ export function setFilter(value) {
   return { type: SET_FILTER, value };
 }
 
-export function createUser(value) {
-  return { type: CREATE_USER, value };
+export function setUser(user) {
+  return { type: SET_USER, user };
 }
 
 export function setFavmov(value) {
-  return { type: SET_FAVMOV, value };
+  return { type: ADD_FAVMOV, value };
 }
 
 export function deleteFavmov(value) {
