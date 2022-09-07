82feafb33cfbe2fc282a50d57e2450314bd95804
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-07 17:25:08 +0200

    Remove import Nav from react-bootstrap

--- src/components/navbar-user-view/navbar-user-view.jsx
+++ src/components/navbar-user-view/navbar-user-view.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Container, Navbar, NavDropdown } from 'react-bootstrap';
 import { NavbarView } from '../navbar-view/navbar-view';
 import { setUser } from '../../actions/actions';
 
