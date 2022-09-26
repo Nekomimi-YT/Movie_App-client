f831c91da8a338e9e8c31766f30676d1170ab579
Nekomimi-YT <nekomimi.snowcrash@gmail.com>
2022-09-26 18:13:18 +0200

    Import scss and remove unnecessary classname

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { Form } from 'react-bootstrap';
-
 import { setFilter } from '../../actions/actions';
+import './visibility-filter-input.scss';
 
 function VisibilityFilterInput(props) {
   return <Form.Control
     onChange={e => props.setFilter(e.target.value)}
     value={props.visibilityFilter}
     placeholder="Search"
-    className="input-focus input-focus-border-color"  //https://getbootstrap.com/docs/5.1/forms/form-control/
+    className="input-focus"  //https://getbootstrap.com/docs/5.1/forms/form-control/
   />;
 }
 
